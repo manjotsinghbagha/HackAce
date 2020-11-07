23b6d8a2ffbfbaa6f5679b865fd8c44bf220347c
manjotsinghbagha <jotroop2001@gmail.com>
2020-11-07 13:25:36 +0530

    AUTH added
    need html files

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    hight_in_cm = models.PositiveIntegerField(null=True)
+    wight_in_kg = models.PositiveIntegerField(null=True)
+    
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
